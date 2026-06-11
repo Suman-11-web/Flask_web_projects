183779109469e999ba6fda8c6804ba3551e412c3
Suman m <sumans48750@gmail.com>
2026-06-11 23:07:12 +0530

    Add files via upload

---
+++ web_project7/app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    message = ""
+
+    if request.method == 'POST':
+        name = request.form['name']
+        message = f"Welcome {name}!"
+
+    return render_template("index.html", message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
+
+
